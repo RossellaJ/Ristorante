6582141614dc3a43a1a3f417e7f7ec949ae6d596
Rossella <corso@10.0.1.196>
2016-11-11 11:24:14 +0100

    con es primeFaces sulle car TABELLA EDIT

---
+++ PrimeFacesRistorante/src/main/java/it/alfasoft/rossella/Car.java
@@ -0,0 +1,49 @@
+package it.alfasoft.rossella;
+
+public class Car {
+	
+	private String id;
+	private String year;
+	private String brand;
+	private String color;
+	
+	public Car(String randomId, String randomBrand, int randomYear,
+			String randomColor, int randomPrice, boolean randomSoldState) {
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	
+
+}
